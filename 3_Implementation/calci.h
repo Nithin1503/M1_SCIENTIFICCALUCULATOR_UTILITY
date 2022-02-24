51d4cc09a0891f708f3d8b410d4d5762d0da3010
Nithin <palaganinithin19@gmail.com>
2022-02-24 19:56:11 +0530

    first commit

---
+++ 3_Implementation/calci.h
@@ -0,0 +1,18 @@
+/**
+ * @file calci.c
+ * @author your name (you@domain.com)
+ * @brief 
+ * @version 0.1
+ * @date 2022-02-24
+ * 
+ * @copyright Copyright (c) 2022
+ * 
+ */
+
+#ifndef calci_H_
+#define calci_H_
+
+#include<stdio.h>
+#include<math.h>
+
+#endif
