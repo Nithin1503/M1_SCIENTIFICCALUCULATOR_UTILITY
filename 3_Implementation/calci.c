4d6e8cd4cc8f2dc61e1d4eb8152a280231a02fee
Nithin <palaganinithin19@gmail.com>
2022-02-24 19:55:16 +0530

    done

--- 3_Implementation/calci.c
+++ 3_Implementation/calci.c
@@ -1,3 +1,14 @@
+/**
+ * @file calci.c
+ * @author your name (you@domain.com)
+ * @brief 
+ * @version 0.1
+ * @date 2022-02-24
+ * 
+ * @copyright Copyright (c) 2022
+ * 
+ */
+
 #include<stdio.h>
 #include<math.h>
 int main()
